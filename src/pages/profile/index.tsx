8181199b7d79a80fd869ca3cf4e6ca94fe481328
Jackson Wambolt <jtwambolt@gmail.com>
2021-10-18 23:45:16 -0500

    Fix username field alignment

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -201,7 +201,6 @@ class ProfilePage extends Component<unknown, ProfilePageState> {
                     valid: await this.isFormValid(),
                   })
                 }}
-                className={styles.profileField}
               />
             </div>
             <Button
