44460a391b6cc022388fdb72a9dc64c69e0c0701
Jackson Wambolt <jtwambolt@gmail.com>
2021-11-23 22:32:49 -0600

    Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,24 @@
 import "Styles/globals.scss"
 import { BlopNavbar } from "Components/BlopNavbar"
+import Head from "next/head"
 
 import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
   // this is the body component
   return (
-    <div id="blop">
-      <BlopNavbar />
-      <div id="blopContent">
-        <Component {...pageProps} />
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/dotsLogo.ico" />
+        <title>Blop</title>
+      </Head>
+      <div id="blop">
+        <BlopNavbar />
+        <div id="blopContent">
+          <Component {...pageProps} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 export default MyApp
